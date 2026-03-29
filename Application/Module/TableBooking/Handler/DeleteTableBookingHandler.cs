093a4d2903565052bb4f4af849daf4b908798295
FajilChauhan <chauhanfajil07@gmail.com>
2026-03-30 01:21:37 +0530

    add DeleteHandler

---
+++ Application/Module/TableBooking/Handler/DeleteTableBookingHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SafnamBackend.Domain.Interface;
+
+namespace SafnamBackend.Application.Module.RoomBooking.Handler
+{
+    public class DeleteTableBookingHandler : IRequestHandler<DeleteTableBookingCommand, string>
+    {
+        private readonly ITableBookingRepository _repo;
+
+        public DeleteTableBookingHandler(ITableBookingRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<string> Handle(DeleteTableBookingCommand request, CancellationToken cancellationToken)
+        {
+            await _repo.DeleteAsync(request.Id);
+            return "Deleted";
+        }
+    }
+}
